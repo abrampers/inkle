30f5ae430b94fbb19dfb26f27a1e7bfe58faa738
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-03-26 16:07:55 +0700

    Created getEvent unit tests

--- logging/eventlogmanager_test.go
+++ logging/eventlogmanager_test.go
@@ -152,7 +152,221 @@ func TestCreateEvent(t *testing.T) {
 // 	for _, test := range tests {
 // 	}
 // }
-//
+
+func TestGetEvent(t *testing.T) {
+	currtime := time.Now()
+	tests := []struct {
+		ipsource, tcpsource, ipdest, tcpdest string
+		events                               []*EventLog
+		idx                                  int
+	}{
+		{
+			ipsource:  "::1",
+			tcpsource: "58108",
+			ipdest:    "::1",
+			tcpdest:   "8000",
+			events: []*EventLog{
+				&EventLog{
+					tstart:      currtime,
+					servicename: "helloworld.Greeter",
+					methodname:  "SayHello",
+					ipsource:    "::1",
+					tcpsource:   "58108",
+					ipdest:      "::1",
+					tcpdest:     "8000",
+					info:        "Request",
+				},
+			},
+			idx: 0,
+		},
+		{
+			ipsource:  "127.0.0.1",
+			tcpsource: "58108",
+			ipdest:    "::1",
+			tcpdest:   "8000",
+			events: []*EventLog{
+				&EventLog{
+					tstart:      currtime,
+					servicename: "helloworld.Greeter",
+					methodname:  "SayHello",
+					ipsource:    "::1",
+					tcpsource:   "58108",
+					ipdest:      "::1",
+					tcpdest:     "8000",
+					info:        "Request",
+				},
+			},
+			idx: -1,
+		},
+		{
+			ipsource:  "::1",
+			tcpsource: "58100",
+			ipdest:    "::1",
+			tcpdest:   "8000",
+			events: []*EventLog{
+				&EventLog{
+					tstart:      currtime,
+					servicename: "helloworld.Greeter",
+					methodname:  "SayHello",
+					ipsource:    "::1",
+					tcpsource:   "58108",
+					ipdest:      "::1",
+					tcpdest:     "8000",
+					info:        "Request",
+				},
+			},
+			idx: -1,
+		},
+		{
+			ipsource:  "::1",
+			tcpsource: "58108",
+			ipdest:    "127.0.0.1",
+			tcpdest:   "8000",
+			events: []*EventLog{
+				&EventLog{
+					tstart:      currtime,
+					servicename: "helloworld.Greeter",
+					methodname:  "SayHello",
+					ipsource:    "::1",
+					tcpsource:   "58108",
+					ipdest:      "::1",
+					tcpdest:     "8000",
+					info:        "Request",
+				},
+			},
+			idx: -1,
+		},
+		{
+			ipsource:  "::1",
+			tcpsource: "58108",
+			ipdest:    "::1",
+			tcpdest:   "9000",
+			events: []*EventLog{
+				&EventLog{
+					tstart:      currtime,
+					servicename: "helloworld.Greeter",
+					methodname:  "SayHello",
+					ipsource:    "::1",
+					tcpsource:   "58108",
+					ipdest:      "::1",
+					tcpdest:     "8000",
+					info:        "Request",
+				},
+			},
+			idx: -1,
+		},
+		{
+			ipsource:  "::1",
+			tcpsource: "58108",
+			ipdest:    "::1",
+			tcpdest:   "8000",
+			events: []*EventLog{
+				&EventLog{
+					tstart:      currtime,
+					servicename: "helloworld.Greeter",
+					methodname:  "SayHello",
+					ipsource:    "::1",
+					tcpsource:   "58108",
+					ipdest:      "::1",
+					tcpdest:     "8000",
+					info:        "Request",
+				},
+			},
+			idx: 1,
+		},
+		{
+			ipsource:  "127.0.0.1",
+			tcpsource: "58108",
+			ipdest:    "::1",
+			tcpdest:   "8000",
+			events: []*EventLog{
+				&EventLog{},
+				&EventLog{
+					tstart:      currtime,
+					servicename: "helloworld.Greeter",
+					methodname:  "SayHello",
+					ipsource:    "::1",
+					tcpsource:   "58108",
+					ipdest:      "::1",
+					tcpdest:     "8000",
+					info:        "Request",
+				},
+			},
+			idx: -1,
+		},
+		{
+			ipsource:  "::1",
+			tcpsource: "58100",
+			ipdest:    "::1",
+			tcpdest:   "8000",
+			events: []*EventLog{
+				&EventLog{},
+				&EventLog{
+					tstart:      currtime,
+					servicename: "helloworld.Greeter",
+					methodname:  "SayHello",
+					ipsource:    "::1",
+					tcpsource:   "58108",
+					ipdest:      "::1",
+					tcpdest:     "8000",
+					info:        "Request",
+				},
+			},
+			idx: -1,
+		},
+		{
+			ipsource:  "::1",
+			tcpsource: "58108",
+			ipdest:    "127.0.0.1",
+			tcpdest:   "8000",
+			events: []*EventLog{
+				&EventLog{},
+				&EventLog{
+					tstart:      currtime,
+					servicename: "helloworld.Greeter",
+					methodname:  "SayHello",
+					ipsource:    "::1",
+					tcpsource:   "58108",
+					ipdest:      "::1",
+					tcpdest:     "8000",
+					info:        "Request",
+				},
+			},
+			idx: -1,
+		},
+		{
+			ipsource:  "::1",
+			tcpsource: "58108",
+			ipdest:    "::1",
+			tcpdest:   "9000",
+			events: []*EventLog{
+				&EventLog{},
+				&EventLog{
+					tstart:      currtime,
+					servicename: "helloworld.Greeter",
+					methodname:  "SayHello",
+					ipsource:    "::1",
+					tcpsource:   "58108",
+					ipdest:      "::1",
+					tcpdest:     "8000",
+					info:        "Request",
+				},
+			},
+			idx: -1,
+		},
+	}
+
+	for _, test := range tests {
+		elm := &eventLogManager{events: test.events}
+		event, idx := elm.getEvent(test.ipsource, test.tcpsource, test.ipdest, test.tcpdest)
+		if idx != test.idx {
+			t.Errorf("getEvent returns wrong index. Expected '%d' got '%d'.", test.idx, idx)
+		} else if idx != -1 && event != elm.events[idx] {
+			t.Error("getEvent returns incorrect pointer.")
+		}
+	}
+}
+
 // func TestCleanupExpiredRequests(t *testing.T) {
 // 	tests := []struct {
 // 		initialevents []EventLog

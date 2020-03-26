085a3346db9726f7604e1b1a79b91025eb872df7
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-03-26 20:05:26 +0700

    Implement getEvent

--- logging/eventlogmanager.go
+++ logging/eventlogmanager.go
@@ -5,8 +5,8 @@ import (
 )
 
 type EventLogManager interface {
-	CreateEvent(timestamp time.Time, servicename string, methodname string, ipsource string, tcpsource string, ipdest string, tcpdest string)
-	InsertResponse(timestamp time.Time, ipsource string, tcpsource string, ipdest string, tcpdest string, grpcstatuscode string)
+	CreateEvent(timestamp time.Time, servicename string, methodname string, ipsource string, tcpsource uint16, ipdest string, tcpdest uint16)
+	InsertResponse(timestamp time.Time, ipsource string, tcpsource uint16, ipdest string, tcpdest uint16, grpcstatuscode string)
 	CleanupExpiredRequests()
 }
 
@@ -19,19 +19,32 @@ func NewEventLogManager(tticker *time.Ticker) EventLogManager {
 	return &eventLogManager{tticker: tticker}
 }
 
-func (m *eventLogManager) CreateEvent(timestamp time.Time, servicename string, methodname string, ipsource string, tcpsource string, ipdest string, tcpdest string) {
+func (m *eventLogManager) CreateEvent(timestamp time.Time, servicename string, methodname string, ipsource string, tcpsource uint16, ipdest string, tcpdest uint16) {
 	e := NewEventLog(timestamp, servicename, methodname, ipsource, tcpsource, ipdest, tcpdest, "Request")
 	m.events = append(m.events, e)
 }
 
-func (m *eventLogManager) InsertResponse(timestamp time.Time, ipsource string, tcpsource string, ipdest string, tcpdest string, grpcstatuscode string) {
+func (m *eventLogManager) InsertResponse(timestamp time.Time, ipsource string, tcpsource uint16, ipdest string, tcpdest uint16, grpcstatuscode string) {
+}
+
+func (m *eventLogManager) getEvent(ipsource string, tcpsource uint16, ipdest string, tcpdest uint16) (event *EventLog, idx int) {
+	for i, event := range m.events {
+		if event.isMatchingRequest(ipdest, tcpdest) {
+			return event, i
+		}
+	}
+	return nil, -1
+}
+
+func (m *eventLogManager) removeEvent(idx int) error {
+	return nil
 }
 
 func (m *eventLogManager) CleanupExpiredRequests() {
-	for _ = range m.tticker.C {
-		m.removeExpiredEvents()
+	for currtime := range m.tticker.C {
+		m.removeExpiredEvents(currtime)
 	}
 }
 
-func (m *eventLogManager) removeExpiredEvents() {
+func (m *eventLogManager) removeExpiredEvents(currtime time.Time) {
 }

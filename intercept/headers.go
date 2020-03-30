e0a6ee18a5ae434f4117245f39d6cb6672fd79c4
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-03-30 20:25:23 +0700

    Implement Headers

---
+++ intercept/headers.go
@@ -0,0 +1,29 @@
+package intercept
+
+import (
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/hpack"
+)
+
+var he headers = headers{headers: make(map[string]string)}
+var decoder *hpack.Decoder = hpack.NewDecoder(4096, he.readHeaderField)
+
+type headers struct {
+	headers map[string]string
+}
+
+func Headers(headersframe http2.HeadersFrame) map[string]string {
+	he.clean()
+	decoder.Write(headersframe.HeaderBlockFragment())
+	return he.headers
+}
+
+func (h *headers) clean() {
+	for key := range h.headers {
+		delete(h.headers, key)
+	}
+}
+
+func (h *headers) readHeaderField(f hpack.HeaderField) {
+	h.headers[f.Name] = f.Value
+}

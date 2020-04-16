9517e49fe7cbeb96079427c6cc3f105bcd6b7475
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-04-16 13:26:24 +0700

    Improve code coverage

--- inkle.go
+++ inkle.go
@@ -55,6 +55,29 @@ func validateResponseFrameHeaders(headers map[string]string) error {
 	return nil
 }
 
+func handlePacket(elm logging.EventLogManager, packet http2.InterceptedPacket) string {
+	headers := http2.Headers(packet.HTTP2)
+	// Check whether this request is response or not
+	if err := validateRequestFrameHeaders(headers); err == nil {
+		http2.State.UpdateState(packet.SrcIP.String(), uint16(packet.SrcTCP), packet.DstIP.String(), uint16(packet.DstTCP), headers)
+		headers = http2.State.Headers(packet.SrcIP.String(), uint16(packet.SrcTCP), packet.DstIP.String(), uint16(packet.DstTCP))
+		servicename, methodname, err := utils.ParseGrpcPath(headers[":path"])
+		if err != nil {
+			return ""
+		}
+		return elm.CreateEvent(time.Now(), servicename, methodname, packet.SrcIP.String(), uint16(packet.SrcTCP), packet.DstIP.String(), uint16(packet.DstTCP))
+	} else if err := validateResponseFrameHeaders(headers); err == nil {
+		http2.State.UpdateState(packet.SrcIP.String(), uint16(packet.SrcTCP), packet.DstIP.String(), uint16(packet.DstTCP), headers)
+		headers = http2.State.Headers(packet.SrcIP.String(), uint16(packet.SrcTCP), packet.DstIP.String(), uint16(packet.DstTCP))
+		statuscode, ok := headers["grpc-status"]
+		if !ok {
+			statuscode = "-1"
+		}
+		return elm.InsertResponse(time.Now(), packet.SrcIP.String(), uint16(packet.SrcTCP), packet.DstIP.String(), uint16(packet.DstTCP), statuscode)
+	}
+	return ""
+}
+
 func main() {
 	flag.Parse()
 
@@ -78,24 +101,6 @@ func main() {
 	go elm.CleanupExpiredRequests()
 
 	for packet := range interceptor.Packets() {
-		headers := http2.Headers(packet.HTTP2)
-		// Check whether this request is response or not
-		if err := validateRequestFrameHeaders(headers); err == nil {
-			http2.State.UpdateState(packet.SrcIP.String(), uint16(packet.SrcTCP), packet.DstIP.String(), uint16(packet.DstTCP), headers)
-			headers = http2.State.Headers(packet.SrcIP.String(), uint16(packet.SrcTCP), packet.DstIP.String(), uint16(packet.DstTCP))
-			servicename, methodname, err := utils.ParseGrpcPath(headers[":path"])
-			if err != nil {
-				continue
-			}
-			elm.CreateEvent(time.Now(), servicename, methodname, packet.SrcIP.String(), uint16(packet.SrcTCP), packet.DstIP.String(), uint16(packet.DstTCP))
-		} else if err := validateResponseFrameHeaders(headers); err == nil {
-			http2.State.UpdateState(packet.SrcIP.String(), uint16(packet.SrcTCP), packet.DstIP.String(), uint16(packet.DstTCP), headers)
-			headers = http2.State.Headers(packet.SrcIP.String(), uint16(packet.SrcTCP), packet.DstIP.String(), uint16(packet.DstTCP))
-			statuscode, ok := headers["grpc-status"]
-			if !ok {
-				statuscode = "-1"
-			}
-			elm.InsertResponse(time.Now(), packet.SrcIP.String(), uint16(packet.SrcTCP), packet.DstIP.String(), uint16(packet.DstTCP), statuscode)
-		}
+		handlePacket(elm, packet)
 	}
 }
